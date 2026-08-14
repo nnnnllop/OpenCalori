63f2df6a69bae98d5c4c63703a36e453218a9f03
nnnnllop <nnnlll000777p@gmail.com>
2026-08-14 12:08:18 +0300

    v0.3.0: final stabilization and release notes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.opencalori.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 13
-        versionName = "0.2.10"
+        versionCode = 14
+        versionName = "0.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
